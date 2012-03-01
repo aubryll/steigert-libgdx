45dea660f6ca05ae12cb74d76399102d86ba1675
steigert <steigert@gmail.com>
2012-03-01 21:38:57 +0000

    Commit for the post #4

--- tyrian-game/src/com/blogspot/steigert/tyrian/screens/MenuScreen.java
+++ tyrian-game/src/com/blogspot/steigert/tyrian/screens/MenuScreen.java
@@ -14,7 +14,8 @@ public class MenuScreen
         AbstractScreen
 {
     // setup the dimensions of the menu buttons
-    private static final float BUTTON_WIDTH = 300f, BUTTON_HEIGHT = 60f;
+    private static final float BUTTON_WIDTH = 300f;
+    private static final float BUTTON_HEIGHT = 60f;
     private static final float BUTTON_SPACING = 10f;
 
     public MenuScreen(
@@ -34,7 +35,7 @@ public class MenuScreen
 
         // label "welcome"
         Label welcomeLabel = new Label( "Welcome to Tyrian for Android!", getSkin() );
-        welcomeLabel.x = ( width / 2 );
+        welcomeLabel.x = ( ( width - welcomeLabel.width ) / 2 );
         welcomeLabel.y = ( currentY + 100 );
         stage.addActor( welcomeLabel );
 
